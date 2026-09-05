b6a1dd654c1f8a2a88c65418c06ff6ba5f8fe740
Raihan Ahmed <raiihan2500@gmail.com>
2026-09-05 16:27:03 +0600

    up

--- codeforces/src/App.jsx
+++ codeforces/src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
-import SearchBar from './SearchBar'
-import Profile from './Profile'
-import { getUser } from './api'
-import { useState } from 'react'
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import Profile from "./Profile";
+import { getUser } from "./api";
 
 function App() {
   const [handle, setHandle] = useState("");
@@ -10,35 +9,31 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(()=>{
+  async function searchHandle(value) {
     setLoading(true);
-    async function search(){
-      try{
-        const data = await getUser(handle);
-        setUser(data);
-        console.log(data);
-      }catch(err){
-        setError(err.message);
-        setUser(null);
-      } finally{
-        setLoading(false);
-      }
-    }  
-    search();
-  }, [handle]);
+    setError(null);
+
+    try {
+      const data = await getUser(value);
+      setUser(data);
+      setHandle(value);
+    } catch (err) {
+      setError(err.message);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <main>
+      <SearchBar searchHandle={searchHandle} />
 
-      <SearchBar 
-        setHandle = {setHandle}
-        />
-      
       {loading && <p>Searching...</p>}
-      {error && <p>{error}</p> }
-      {!loading && !error && user && <Profile user = {user} /> }
+      {error && <p>{error}</p>}
+      {!loading && !error && user && <Profile user={user} />}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- codeforces/src/SearchBar.jsx
+++ codeforces/src/SearchBar.jsx
@@ -1,25 +1,32 @@
 import { useRef } from "react";
+
 function SearchBar(props) {
   const userInputRef = useRef();
-  function search(){
-    const value = userInputRef.current.value;
-    props.setHandle(value);
+
+  function search() {
+    const value = userInputRef.current.value.trim();
+
+    if (value === "") return;
+
+    props.searchHandle(value);
   }
-  
+
   return (
     <div>
-      <input 
+      <input
         ref={userInputRef}
-        type="text" placeholder='Enter the username' 
-        onKeyDown={(event)=>{
-          if(event.key === "Enter"){
+        type="text"
+        placeholder="Enter the username"
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
             search();
           }
         }}
-     />
-      <button onClick={search} >Search</button>
+      />
+
+      <button onClick={search}>Search</button>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
